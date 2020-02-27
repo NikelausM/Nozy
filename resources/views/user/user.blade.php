46d8b82fef0020ff4112f13da1e6390478dd25e8
jnickm@gmail.com <jnickm@gmail.com>
2020-02-27 13:31:14 -0700

    changed css of buttons

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -4,16 +4,16 @@
 <div class="masthead">
 	<div class="container h-100">
 		<br></br>
-		<div class="row h-100 align-items-center">
-			<div style="top: 5%;" class="col-12">
+		<div class="row h-75 align-items-center">
+			<div style="top: 25%;" class="col-12">
 				<h1 class="font-weight-bold"><font color="black">Hello {{$user->name}}</font></h1>
 				<h1 class="font-weight-bold"><font color="black">Your email is: {{$user->email}}</font></h1>
 				<h1 class="font-weight-bold"><font color="black">Your description is: {{$user->profile->description}}</font></h1>
 				<h2 class="font-weight-bold"><font color="black">Communities managed by you are:</font></h2>
 				@foreach($communities as $community)
-				<div class="row">
+				<div class="row" style="padding: 10px">
 					<div class="col-sm-6 col-md-4">
-						<div class="thumbnail">
+						<div class="thumbnail shadow">
 							<h3><font color="black">{{ $community->profile->name }}</font></h3>
 							<div class="caption">
 								<p><font color="black">{{ $community->profile->description }}</font></p>
