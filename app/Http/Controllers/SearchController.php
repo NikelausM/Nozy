6a588f0c9273a5b69a1a0681cda96241ddfdff8e
jnickm@gmail.com <jnickm@gmail.com>
2020-03-19 12:13:47 -0600

    Added dynamic search functionality

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Profile;
 use App\User;
 use App\Community;
 use App\Post;
@@ -17,7 +18,110 @@ class SearchController extends Controller
      */
     public function index()
     {
-      return view('search.search', ['users' => User::all(), 'communities' => Community::all(), 'posts' => Post::all()]);
+      return view('search.search');
+    }
+
+    /**
+     * Filter search results.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function search(Request $request)
+    {
+      Log::info('Trying to return data!');
+      $communities;
+      // Live search bar filter
+      if ($request->ajax()) {
+        $output = "";
+        $query = $request->get('query');
+
+        // Get matching profiles
+        $profile_ids = DB::table('profile')->where('name', 'LIKE', '%'.$query."%")
+                            ->orWhere('description', 'LIKE', '%'.$query."%")->get()->pluck('id');
+
+        // Get corresponding communities
+        $communities = \App\Community::whereIn('profile_id',$profile_ids)->get();
+
+        // Get corresponding users
+        $users = \App\User::whereIn('profile_id',$profile_ids)->get();
+
+        // Get matching Posts
+        $posts = Post::where('subject', 'LIKE', '%'.$query."%")
+                      ->orWhere('body', 'LIKE', '%'.$query."%")->get();
+
+      }
+
+      $community_thumbnails = '';
+      $user_thumbnails = '';
+      $post_thumbnails = '';
+
+      foreach($communities as $community) {
+        //$community_thumbnails .= '<p>new community</p>';
+        $community_thumbnails .= "
+          <div class='row' style='padding: 10px'>
+          	<div class='col-sm-6 col-md-4'>
+          		<div class='thumbnail shadow'>
+          			<h3><font color='black'>".$community->profile->name."</font></h3>
+          			<div class='caption'>
+          				<p><font color='black'>".$community->profile->description."</font></p>
+          				<div>
+                    <form method='GET' action=".route('community.show',$community->id)." accept-charset='UTF-8'>
+                      <button type='submit' class='btn btn-primary'>Go to community</button>
+                    </form>
+          				</div>
+          			</div>
+          		</div>
+          	</div>
+          </div>
+          ";
+      }
+
+      foreach($users as $user) {
+        $user_thumbnails .= "
+          <div class='row' style='padding: 10px'>
+            <div class='col-sm-6 col-md-4'>
+              <div class='thumbnail shadow'>
+                <h3><font color='black'>".$user->profile->name."</font></h3>
+                <div class='caption'>
+                  <p><font color='black'>".$user->profile->description."</font></p>
+                  <div>
+                    <form method='GET' action=".route('user.show',$user->id)." accept-charset='UTF-8'>
+                      <button type='submit' class='btn btn-primary'>Go to user</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          ";
+      }
+
+      foreach($posts as $post) {
+        $post_thumbnails .= "
+          <div class='row' style='padding: 10px'>
+            <div class='col-sm-6 col-md-4'>
+              <div class='thumbnail shadow'>
+                <h3><font color='black'>".$post->subject."</font></h3>
+                <div class='caption'>
+                  <p><font color='black'>".$post->body."</font></p>
+                  <div>
+                    <form method='GET' action=".route('post.show',$post->id)." accept-charset='UTF-8'>
+                      <button type='submit' class='btn btn-primary'>Go to post</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          ";
+      }
+
+      Log::info('returning data!');
+      return response()->json([
+        'community_thumbnails' => $community_thumbnails,
+        'user_thumbnails' => $user_thumbnails,
+        'post_thumbnails' => $post_thumbnails,
+      ]);
     }
 
     /**
--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -7,20 +7,50 @@
 		<div class="row h-75 align-items-center">
 			<div style="top: 20%;" class="col-12">
 				<h1 class="font-weight-bold">Search Nozy!</h1>
+				<div class="form-group">
+ 					<input type="text" name="search" id="search" class="form-control" placeholder="Search communities, users, posts..." />
+				</div>
+				<h1><span id="testid"></span></h1>
+        <h2 class="font-weight-bold">Communities</h2>
+				<div id='community_thumbnails'></div>
 				<h2 class="font-weight-bold">Users</h2>
-				@foreach($users as $user)
-				@include('layouts.user_box')
-				@endforeach
-				<h2 class="font-weight-bold">Communities</h2>
-				@foreach($communities as $community)
-				@include('layouts.community_box')
-				@endforeach
-				<h2 class="font-weight-bold"><font color="black">Posts</font></h2>
-				@foreach($posts as $post)
-				@include('layouts.post_box')
-				@endforeach
+				<div id='user_thumbnails'></div>
+				<h2 class="font-weight-bold">Posts</h2>
+				<div id='post_thumbnails'></div>
 			</div>
 		</div>
 	</div>
 </div>
+
+<script>
+
+$(document).ready(function(){
+
+ fetch_customer_data();
+
+ function fetch_customer_data(query = '')
+ {
+  $.ajax({
+   url:"{{ route('search.search') }}",
+   method:'GET',
+   data:{query:query},
+   dataType:'json',
+   success:function(data)
+   {
+		 $('#testid').html('success');
+		 $('#community_thumbnails').html(data.community_thumbnails);
+		 $('#user_thumbnails').html(data.user_thumbnails);
+		 $('#post_thumbnails').html(data.post_thumbnails);
+		 //window.location.reload();
+   }
+  })
+ }
+
+ $(document).on('keyup', '#search', function(){
+  var query = $(this).val();
+  fetch_customer_data(query);
+ });
+});
+
+</script>
 @endsection
