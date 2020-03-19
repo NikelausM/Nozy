e035aa5da64681800360f5181c04bab5d09c9410
jnickm@gmail.com <jnickm@gmail.com>
2020-03-18 19:36:15 -0600

    Added search page. Lists users, communities, post. Does not filter list yet though

--- app/Community.php
+++ app/Community.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Community extends Profile
 {
-	
+
 	protected $table = "community";
-	
+
 	protected $fillable = [
            'profile_id',
            'manager_user_id',
@@ -17,21 +17,21 @@ class Community extends Profile
     //public $incrementing = false;
     //public $keyType = 'string';
     public $timestamps = false;
-    
+
     /*
 	public function getRouteKeyName() {
 			return 'name';
 	}
 	*/
-    
+
     # community extends profile
     public function profile() {
 		return $this->belongsTo('App\Profile', 'profile_id', 'id');
 	}
-	
+
 	# user managing community
     public function user() {
 		return $this->belongsTo('App\User', 'manager_user_id', 'id');
 	}
-	
+
 }
