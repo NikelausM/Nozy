7b9b75a35c505b0fc6be2e1ac579488c322c2f5f
jnickm@gmail.com <jnickm@gmail.com>
2020-03-29 02:41:33 -0600

    Added notifications pop-up (modal) to notifications nav-link

---
+++ resources/views/notification/notifications.blade.php
@@ -0,0 +1,9 @@
+<div class="modal-header">
+	<h5 class="modal-title" id="exampleModalLabel">Notifications</h5>
+	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	  <span aria-hidden="true">&times;</span>
+	</button>
+</div>
+@foreach(Auth::guard('profile')->user()->notifications as $notification)
+	<p style="text-align: center">{{$notification->message}}</p>
+@endforeach
