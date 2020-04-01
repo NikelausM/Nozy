e3ab643c752f7f8b1a14a37cdad95360324c59a7
jnickm@gmail.com <jnickm@gmail.com>
2020-04-01 04:02:19 -0600

    Added space between like and dislike buttons

--- resources/views/post/dislike_post_button.blade.php
+++ resources/views/post/dislike_post_button.blade.php
@@ -1,5 +1,5 @@
 
-<form action={{route('post.dislike', $post)}} method="post" style="float:right">
+<form action={{route('post.dislike', $post)}} method="post" style="float:right;margin:5px">
 	<button style="margin-top: 5px;" class="btn btn-primary" type="submit">Dislike post</button>
 	{{csrf_field()}}
 </form>
--- resources/views/post/like_post_button.blade.php
+++ resources/views/post/like_post_button.blade.php
@@ -1,5 +1,5 @@
 
-<form action={{route('post.like', $post)}} method="post" style="float:right">
+<form action={{route('post.like', $post)}} method="post" style="float:right;margin:5px">
 	<button style="margin-top: 5px;" class="btn btn-primary" type="submit">Like post</button>
 	{{csrf_field()}}
 </form>
