7c891794ce1bc66d062d704e02d68b473f6e3197
jnickm@gmail.com <jnickm@gmail.com>
2020-03-31 22:39:25 -0600

    Fixing new merge issue

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -11,13 +11,9 @@
 <link href="https://fonts.googleapis.com/css?family=Oswald:400,600,700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
 
-<<<<<<< HEAD
+// Trying to fix stuff
 <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" type="text/css">
 <link rel="stylesheet" href="{{ URL::asset('css/G15.css') }}" type="text/css">
-=======
-<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('css/g15.css') }}">
->>>>>>> nicolas
 
 <?php Session::put('unique_id', 1); // Reset unique id used by tags?>
 <!-- Styles -->
